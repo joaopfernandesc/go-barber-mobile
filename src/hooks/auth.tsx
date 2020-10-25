a27f782e66d9d13e3f18235a4fa9ec3aa50b35fb
João <joaopfernandesc@gmail.com>
2020-10-25 15:17:52 -0300

    Finished dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -45,6 +45,7 @@ const AuthProvider: React.FC = ({ children }) => {
         '@GoBarber:user',
       ]);
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -66,6 +67,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:Token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
 
+import Icon from 'react-native-vector-icons/Feather';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 import {
   Container,
@@ -10,15 +12,44 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
+  ProvidersListTitle,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const { signOut, user } = useAuth();
   const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
 
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
   }, [navigate]);
+
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointment', { providerId });
+    },
+    [navigate],
+  );
 
   return (
     <Container>
@@ -31,6 +62,36 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        renderItem={({ item: provider }) => (
+          <ProviderContainer
+            onPress={() => {
+              navigateToCreateAppointment(provider.id);
+            }}>
+            <ProviderAvatar source={{ uri: provider.avatar_url }} />
+
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+            </ProviderInfo>
+
+            <ProviderMeta>
+              <Icon name="calendar" size={14} color="#ff9000" />
+              <ProviderMetaText>Segunda à sexta</ProviderMetaText>
+            </ProviderMeta>
+
+            <ProviderMeta>
+              <Icon name="clock" size={14} color="#ff9000" />
+              <ProviderMetaText>8h às 18h</ProviderMetaText>
+            </ProviderMeta>
+          </ProviderContainer>
+        )}
+      />
     </Container>
   );
 };
